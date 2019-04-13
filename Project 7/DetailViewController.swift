9127011183215fcc17368263f2ac113e2af82475
nadyapost <nadyapostr@gmail.com>
2019-04-13 22:34:24 +1000

    Filter petitions working.

--- Project 7/DetailViewController.swift
+++ Project 7/DetailViewController.swift
@@ -26,11 +26,18 @@ class DetailViewController: UIViewController {
       let html = """
       <html>
       <head>
+      <script src="https://code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+      <script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <style> body { font-size: 150%; } </style>
       </head>
       <body>
-      \(detailItem.body)
+        <h1 class="ui header">\(detailItem.title)</h1>
+        <p>
+          \(detailItem.body)
+        </p>
+        <p><strong><i class="heart icon"></i> Signatures: \(detailItem.signatureCount)</strong></p>
       </body>
       </html>
       """
